d6ecd1e8b4e873237c3d6c99c28fdedb521d214c
GabagoolOverHere <vicmartindev@gmail.com>
2021-08-07 11:39:29 +0200

    change on footer

--- embeds.py
+++ embeds.py
@@ -4,7 +4,7 @@ import discord
 def construct_embed(self, embed: discord.Embed, author: str, icon: str, commander_name: str, max_troop_size: str,
                     troops_scanned: str, troops: list,
                     thumbnail_url='https://tinyurl.com/38wauca2',
-                    footer='React with \U0001F197 (:ok:) if the scan seems to be correct.\nReact with \U0000274C (:x:) if the scan seems to be incorrect.'):
+                    footer='React with \U0001F197 (:ok:) if the scan seems to be correct.\nReact with \U0000274C (:x:) if the scan seems to be incorrect.\nCheck out clan\'s stats at: http://gabagool.fr/'):
     embed.set_author(name=author, icon_url=icon)
     embed.add_field(name='Commander Name:', value=commander_name)
     embed.add_field(name='Max Troop Size:', value=max_troop_size)
